76175e1f3d65d91cece8a37386a87c93a78f6546
ronyeh <ronyeh@a0ca9acf-f029-0410-a206-658ddece02a0>
2007-03-06 08:04:45 +0000

    Yay. We can now run MOSS through java/perl. =) You need to customize your PATH to include cygwin bash.

--- src/edu/stanford/hci/sourcecode/moss/MOSS.java
+++ src/edu/stanford/hci/sourcecode/moss/MOSS.java
@@ -1,11 +1,14 @@
 package edu.stanford.hci.sourcecode.moss;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import edu.stanford.hci.r3.util.DebugUtils;
+import edu.stanford.hci.sourcecode.DiscoverSourceFiles;
 
 /**
  * <p>
@@ -24,21 +27,44 @@ import edu.stanford.hci.r3.util.DebugUtils;
  */
 public class MOSS {
 
+	private static String buildListOfFiles(File path) {
+		String prefix = path.getPath();
+
+		final DiscoverSourceFiles files = new DiscoverSourceFiles(path);
+		final List<File> listOfFiles = files.getListOfFiles();
+
+		StringBuilder sb = new StringBuilder();
+		for (File f : listOfFiles) {
+			sb.append(prefix + "\\" + f.getName() + " ");
+		}
+
+		return sb.toString().trim();
+	}
+
 	public static void main(String[] args) {
 
 		// run moss through perl
-
 		// moss.pl -l java -d <directoryContainingJavaFiles> <directoryContainingJavaFiles>
-
 		String prefix = "files/growncode/";
-		String projectJavaFiles = prefix + "p_4Corners/Ink.java";
-		String toolkitJavaFiles = prefix + "b_toolkit/Ink.java";
-		
-		
+		// String projectJavaFiles = buildListOfFiles(new File(prefix + "p_4Corners/"));
+		// String toolkitJavaFiles = buildListOfFiles(new File(prefix + "b_PaperToolkit/"));
+		String projectJavaFiles = prefix + "p_4Corners/*.java";
+		String toolkitJavaFiles = prefix + "b_PaperToolkit/*.java";
+		DebugUtils.println(projectJavaFiles);
+		DebugUtils.println(toolkitJavaFiles);
+		String mossCommand = "cmd /c bash --login -c \"cd projects/SourceCodeAnalysis; perl externalBin/moss.pl -l java -m 1000000 -d "
+				+ projectJavaFiles + " " + toolkitJavaFiles + "\"";
+		run(mossCommand);
+		// run("cmd /c dir");
+		// run("cmd /c bash --login -c \"perl -v\"");
+	}
+
+	/**
+	 * R3 v 4Corners http://moss.stanford.edu/results/103112459
+	 */
+	private static void run(String command) {
 		try {
-			
-			//Process proc = Runtime.getRuntime().exec("cmd /c dir");
-			Process proc = Runtime.getRuntime().exec("perl externalBin/moss.pl -l java -d " + projectJavaFiles + " " + toolkitJavaFiles);
+			Process proc = Runtime.getRuntime().exec(command);
 			InputStream inputStream = proc.getInputStream();
 			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
 			InputStream errorStream = proc.getErrorStream();
@@ -59,7 +85,6 @@ public class MOSS {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
