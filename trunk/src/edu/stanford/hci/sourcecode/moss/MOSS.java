c566a9f98d51be0e56786898d907c7e59b1679cc
ronyeh <ronyeh@a0ca9acf-f029-0410-a206-658ddece02a0>
2007-03-28 01:14:26 +0000

    Multicolumn support. =)

--- trunk/src/edu/stanford/hci/sourcecode/moss/MOSS.java
+++ trunk/src/edu/stanford/hci/sourcecode/moss/MOSS.java
@@ -16,6 +16,9 @@ import edu.stanford.hci.sourcecode.DiscoverSourceFiles;
  * system, you should already have Perl in the terminal.
  * 
  * See Alex Aiken's MOSS page for instructions on how to install and use MOSS.
+ * 
+ * We will use MOSS in combination with text search and eyeballing to do our copied code analysis.
+ * The data will be tagged by hand and saved into an Excel spreadsheet.
  * </p>
  * <p>
  * <span class="BSDLicense"> This software is distributed under the <a
@@ -27,7 +30,75 @@ import edu.stanford.hci.sourcecode.DiscoverSourceFiles;
  */
 public class MOSS {
 
-	private static String buildListOfFiles(File path) {
+	public static void main(String[] args) {
+		// String projectName = "4Corners"; // DONE
+		// String projectName = "Anototators"; // DONE
+		// String projectName = "BigBrother"; // DONE
+		// String projectName = "BluJay"; // DONE
+		// String projectName = "ET"; // DONE
+		// String projectName = "GeoPhoto"; // DONE
+		// String projectName = "GGiD"; // DONE
+		String projectName = "Jigsaw";
+		// String projectName = "JSandA"; // DONE
+		// String projectName = "JURMs"; // DONE
+		// String projectName = "KMAT"; // DONE
+		// String projectName = "Melody"; // DONE
+		// String projectName = "O"; // DONE
+		// String projectName = "PollPrecision"; // DONE
+		// String projectName = "SEKS"; // DONE
+		// String projectName = "Team42"; // DONE
+		// String projectName = "ThePenIsMightier"; // DONE
+
+		String baseProjectName = "PaperToolkit";
+		// String baseProjectName = "PaperToolkitDemos";
+		// String baseProjectName = "CS160HelloWorld";
+		// String baseProjectName = "web";
+
+		new MOSS(projectName, baseProjectName);
+	}
+
+	public MOSS(String projectName, String baseProjectName) {
+		// run moss through perl
+		// moss.pl -l java -d <directoryContainingJavaFiles> <directoryContainingJavaFiles>
+		String prefix = "files/growncode/";
+
+		// #1, To compare within a CS160 project, we just submit all the files from that project
+		// #2, To compare between project and the toolkit, we submit two directories
+		// #3, To compare between project and hello world, we submit the two directories also...
+		// Then, we go through the returned web page by hand, and annotate what MOSS thinks are
+		// similarities. We tag these similarities and produce a graph! =)
+		String projectJavaFiles = prefix + "p_" + projectName + "/*.java";
+		String baseJavaFiles = prefix + "b_" + baseProjectName + "/*.java";
+
+		// DebugUtils.println(projectJavaFiles);
+		// DebugUtils.println(baseJavaFiles);
+
+		// The between project matching seems to be one-to-one copies. I'd like to find
+		// one-to-many...should I not use the -d flag?
+		String mossCommand = "cmd /c bash --login -c \"cd projects/SourceCodeAnalysis; perl externalBin/moss.pl -l java -m 1000000 "
+				// + "-d"
+				+ projectJavaFiles + " " + baseJavaFiles + "\"";
+
+		// sanity check on MOSS! submit the same projects
+		// OK, it returns 99% =)
+		// String mossCommand = "cmd /c bash --login -c \"cd projects/SourceCodeAnalysis; perl
+		// externalBin/moss.pl -l java -m 1000000 -d "
+		// + baseJavaFiles + " " + prefix + "b_PaperToolkitClone/*.java\"";
+
+		DebugUtils.println(mossCommand);
+		run(mossCommand);
+
+		// run("cmd /c dir");
+		// run("cmd /c bash --login -c \"perl -v\"");
+	}
+
+	/**
+	 * Doesn't work.
+	 * 
+	 * @param path
+	 * @return
+	 */
+	private String buildListOfFiles(File path) {
 		String prefix = path.getPath();
 
 		final DiscoverSourceFiles files = new DiscoverSourceFiles(path);
@@ -41,28 +112,10 @@ public class MOSS {
 		return sb.toString().trim();
 	}
 
-	public static void main(String[] args) {
-
-		// run moss through perl
-		// moss.pl -l java -d <directoryContainingJavaFiles> <directoryContainingJavaFiles>
-		String prefix = "files/growncode/";
-		// String projectJavaFiles = buildListOfFiles(new File(prefix + "p_4Corners/"));
-		// String toolkitJavaFiles = buildListOfFiles(new File(prefix + "b_PaperToolkit/"));
-		String projectJavaFiles = prefix + "p_4Corners/*.java";
-		String toolkitJavaFiles = prefix + "b_PaperToolkit/*.java";
-		DebugUtils.println(projectJavaFiles);
-		DebugUtils.println(toolkitJavaFiles);
-		String mossCommand = "cmd /c bash --login -c \"cd projects/SourceCodeAnalysis; perl externalBin/moss.pl -l java -m 1000000 -d "
-				+ projectJavaFiles + " " + toolkitJavaFiles + "\"";
-		run(mossCommand);
-		// run("cmd /c dir");
-		// run("cmd /c bash --login -c \"perl -v\"");
-	}
-
 	/**
 	 * R3 v 4Corners http://moss.stanford.edu/results/103112459
 	 */
-	private static void run(String command) {
+	private void run(String command) {
 		try {
 			Process proc = Runtime.getRuntime().exec(command);
 			InputStream inputStream = proc.getInputStream();
